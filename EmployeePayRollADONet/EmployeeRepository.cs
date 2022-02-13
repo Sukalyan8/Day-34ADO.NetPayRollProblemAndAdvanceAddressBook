b0d71e8960b1a7d6ff319a29eaa73f4d3a52fe97
Sukalyan <sukalyandash1920@gmail.com>
2022-02-13 21:59:12 +0530

    UC-5 Aggregate Function Based On Gender

--- EmployeePayRollADONet/EmployeeRepository.cs
+++ EmployeePayRollADONet/EmployeeRepository.cs
@@ -8,7 +8,7 @@ namespace EmployeePayRollADONet
 {
     public class EmployeeRepository
     {
-        // for Database Connection
+        // For Database Connection
         public static string connection = @"Server=.;Database=PayRoll_Service;Trusted_Connection=True;";
         SqlConnection sqlConnection = new SqlConnection(connection);
 
@@ -56,7 +56,7 @@ namespace EmployeePayRollADONet
         {
             //Open Connection
             sqlConnection.Open();
-            string query = "update employee_payroll set basicPay=3670000 where name= 'suresh'";
+            string query = "update employee_payroll set basicPay=3670000 where name= 'rakshi'";
             //Pass query to TSql
             SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
             int result = sqlCommand.ExecuteNonQuery();
@@ -202,11 +202,53 @@ namespace EmployeePayRollADONet
             return nameList;
 
         }
+
+
+        public string AggregateFunctionBasedOnGender(string query)
+        {
+            string nameList = "";
+            try
+            {
+                using (sqlConnection)
+                {
+                    ////query execution
+                    SqlCommand command = new SqlCommand(query, this.sqlConnection);
+                    //open sql connection
+                    sqlConnection.Open();
+
+                    SqlDataReader sqlDataReader = command.ExecuteReader();
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine("TotalSalary: {0} \t MinimumSalary: {1} \t MaximumSalary: {2}AverageSalary: {3} \t Count: {4}", sqlDataReader[0], sqlDataReader[1], sqlDataReader[2], sqlDataReader[3], sqlDataReader[4]);
+                            nameList += sqlDataReader[0] + " " + sqlDataReader[1] + " " + sqlDataReader[2] + " " + sqlDataReader[3] + " " + sqlDataReader[4];
+                        }
+                    }
+                    //close reader
+                    sqlDataReader.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+                sqlConnection.Close();
+            }
+            //returns the count of employee in the list between the given range
+            return nameList;
+
+        }
+
         public void DisplayEmployeeDetails(SqlDataReader sqlDataReader)
         {
             //Read data SqlDataReader and store 
             employeeDataManager.id = sqlDataReader.GetInt32(0);
-            employeeDataManager.name = sqlDataReader["EmployeeName"].ToString();
+            employeeDataManager.name = sqlDataReader["name"].ToString();
             employeeDataManager.salary = Convert.ToDouble(sqlDataReader["salary"]);
             employeeDataManager.Deduction = Convert.ToDouble(sqlDataReader["Deduction"]);
             employeeDataManager.IncomeTax = Convert.ToDouble(sqlDataReader["IncomeTax"]);
@@ -216,8 +258,8 @@ namespace EmployeePayRollADONet
             employeeDataManager.EmployeePhoneNumber = Convert.ToInt64(sqlDataReader["EmployeePhoneNumber"]);
             employeeDataManager.EmployeeDepartment = sqlDataReader["EmployeeDepartment"].ToString();
             employeeDataManager.Address = sqlDataReader["Address"].ToString();
-            employeeDataManager.startDate = Convert.ToDateTime(sqlDataReader["StartDate"]);
-            //Display 
+            employeeDataManager.startDate = Convert.ToDateTime(sqlDataReader["startDate"]);
+            //Display Data
             Console.WriteLine("\nEmployee ID: {0} \t Employee Name: {1} \nBasic Pay: {2} \t Deduction: {3} \t Income Tax: {4} \t Taxable Pay: {5} \t NetPay: {6} \nGender: {7} \t PhoneNumber: {8} \t Department: {9} \t Address: {10} \t Start Date: {11}", employeeDataManager.id, employeeDataManager.name, employeeDataManager.salary, employeeDataManager.Deduction, employeeDataManager.IncomeTax, employeeDataManager.TaxablePay, employeeDataManager.NetPay, employeeDataManager.Gender, employeeDataManager.EmployeePhoneNumber, employeeDataManager.EmployeeDepartment, employeeDataManager.Address, employeeDataManager.startDate);
 
         }
