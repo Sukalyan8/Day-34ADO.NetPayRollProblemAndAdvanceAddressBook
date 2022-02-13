d4e735e26df9d1cc4caa47db9e9ac6a80868281e
Sukalyan <sukalyandash1920@gmail.com>
2022-02-13 21:52:09 +0530

    UC-5 Retrieve By Name

---
+++ EmployeePayRollADONet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeePayRollADONet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------Welcome TO Employee PayRoll Services Using ADO.NET----------");
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+            employeeRepository.GetDataFromSql();
+        }
+    }
+}
